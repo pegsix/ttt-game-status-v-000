f04b5ee3b0288a00bad92ba5fdb782d0dea68af4
Peggy Lee <peggyleesix@gmail.com>
2018-06-07 21:15:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,7 +35,7 @@ def won?(board)
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
+       win_combination
     end
   end
    return false
